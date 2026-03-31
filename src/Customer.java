175276f17b8883e30167db98cba3e1068f84220b
Shivam singh <itzshivamhere@gmail.com>
2026-03-31 19:57:14 +0530

    Update Customer.java
     Added comments, improved invoice format, and enhanced service selection logic

--- src/Customer.java
+++ src/Customer.java
@@ -1,21 +1,26 @@
+// Customer class stores customer details along with their car
 public class Customer {
     private String name;
     private String phone;
     private Car car;
 
-    public String getName() {
-        return name;
-    }
-    public String getPhone() {
-        return phone;
-    }
-    public Car getCar() {
-        return car;
-    }
+    // Constructor to initialize customer details
     public Customer(String name, String phone, Car car) {
         this.name = name;
         this.phone = phone;
         this.car = car;
+    }
 
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public Car getCar() {
+        return car;
     }
 }
